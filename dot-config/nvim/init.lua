8349bc6475a0e7f3d9e1152f1fc2349fe7828f3a
Andy <ngocdunghoangdl@gmail.com>
2024-11-12 09:26:12 +0700

    wip

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -60,6 +60,52 @@ require("lazy").setup({
     "tpope/vim-dispatch",
     "savq/melange",
     { "neoclide/coc.nvim", branch = "release" },
+    --{ "yetone/avante.nvim",
+      --event = "VeryLazy",
+      --lazy = false,
+      --version = false, -- set this if you want to always pull the latest change
+      --opts = {
+        ---- add any opts here
+      --},
+      ---- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+      --build = "make",
+      ---- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+      --dependencies = {
+        --"nvim-treesitter/nvim-treesitter",
+        --"stevearc/dressing.nvim",
+        --"nvim-lua/plenary.nvim",
+        --"MunifTanjim/nui.nvim",
+        ----- The below dependencies are optional,
+        --"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        --"zbirenbaum/copilot.lua", -- for providers='copilot'
+        --{
+          ---- support for image pasting
+          --"HakonHarnes/img-clip.nvim",
+          --event = "VeryLazy",
+          --opts = {
+            ---- recommended settings
+            --default = {
+              --embed_image_as_base64 = false,
+              --prompt_for_file_name = false,
+              --drag_and_drop = {
+                --insert_mode = true,
+              --},
+              ---- required for Windows users
+              --use_absolute_path = true,
+            --},
+          --},
+        --},
+        --{
+          ---- Make sure to set this up properly if you have lazy=true
+          --'MeanderingProgrammer/render-markdown.nvim',
+          --opts = {
+            --file_types = { "markdown", "Avante" },
+          --},
+          --ft = { "markdown", "Avante" },
+        --},
+      --},
+    --}
+
     -- Commented out plugins:
     -- "maximbaz/lightline-ale",
     -- "dense-analysis/ale",
@@ -75,7 +121,10 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = {},
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { 
+    enabled = true,
+    notify = false,
+  },
 })
 
 
